0fdfe8dceb3da4fb710d69b3a448f851f5b7d44c
Gerald Lochner <gerald.lochner@meshmakers.io>
2024-02-12 13:56:53 +0100

    AB#1035 Fix user create/edit/list AB#1036 Fix reset password AB#1038 Fix role create/edit/list

--- src/IdentityServices/SystemApi/v1/Controllers/UsersController.cs
+++ src/IdentityServices/SystemApi/v1/Controllers/UsersController.cs
@@ -1,8 +1,10 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using AutoMapper;
 using IdentityModel;
 using IdentityServerPersistence;
 using Meshmakers.Octo.Backend.IdentityServices.Services;
 using Meshmakers.Octo.Communication.Contracts.DataTransferObjects;
+using Meshmakers.Octo.ConstructionKit.Contracts;
 using Meshmakers.Octo.Runtime.Contracts.RepositoryEntities;
 using Meshmakers.Octo.Services.Common.ApiErrors;
 using Microsoft.AspNetCore.Authorization;
@@ -20,10 +22,11 @@ namespace Meshmakers.Octo.Backend.IdentityServices.SystemApi.v1.Controllers;
 [Route(IdentityServiceConstants.ApiPathPrefix + "/[controller]")]
 [ApiController]
 [ApiVersion(IdentityServiceConstants.ApiVersion1)]
-public class IdentitiesController : ControllerBase
+public class UsersController : ControllerBase
 {
-    private readonly ILogger<IdentitiesController> _logger;
+    private readonly ILogger<UsersController> _logger;
     private readonly RoleManager<RtRole> _roleManager;
+    private readonly IMapper _mapper;
     private readonly IUserEmailInteractionService _userEmailInteractionService;
     private readonly UserManager<RtUser> _userManager;
 
@@ -32,21 +35,24 @@ public class IdentitiesController : ControllerBase
     /// </summary>
     /// <param name="userManager">The storage service of users</param>
     /// <param name="roleManager">The storage service of roles</param>
+    /// <param name="mapper"></param>
     /// <param name="userEmailInteractionService"></param>
     /// <param name="logger">Logger</param>
-    public IdentitiesController(
+    public UsersController(
         UserManager<RtUser> userManager,
         RoleManager<RtRole> roleManager,
+        IMapper mapper,
         IUserEmailInteractionService userEmailInteractionService,
-        ILogger<IdentitiesController> logger)
+        ILogger<UsersController> logger)
     {
         _userManager = userManager;
         _roleManager = roleManager;
+        _mapper = mapper;
         _userEmailInteractionService = userEmailInteractionService;
         _logger = logger;
     }
 
-    // GET system/v1/identities
+    // GET system/v1/users
     /// <summary>
     ///     Returns all existing users
     /// </summary>
@@ -55,17 +61,12 @@ public class IdentitiesController : ControllerBase
     [Authorize(IdentityServiceConstants.IdentityApiReadOnlyPolicy)]
     public IEnumerable<UserDto> Get()
     {
-        var list = new List<UserDto>();
-        foreach (var applicationUser in _userManager.Users)
-        {
-            var userDto = CreateUserDto(applicationUser);
-            list.Add(userDto);
-        }
+        var list = _mapper.Map<List<UserDto>>(_userManager.Users);
 
         return list;
     }
 
-    // GET system/v1/identities/getPaged
+    // GET system/v1/users/getPaged
     /// <summary>
     ///     Returns all existing users
     /// </summary>
@@ -75,9 +76,9 @@ public class IdentitiesController : ControllerBase
     public PagedResult<UserDto> Get([Required] [FromQuery] PagingParams pagingParams)
     {
         var list = new List<UserDto>();
-        foreach (var applicationUser in _userManager.Users.Skip(pagingParams.Skip).Take(pagingParams.Take))
+        foreach (var rtUser in _userManager.Users.Skip(pagingParams.Skip).Take(pagingParams.Take))
         {
-            var userDto = CreateUserDto(applicationUser);
+            var userDto = _mapper.Map<UserDto>(rtUser);
             list.Add(userDto);
         }
 
@@ -93,7 +94,7 @@ public class IdentitiesController : ControllerBase
         return pagedResult;
     }
 
-    // GET system/v1/identities/{userName}
+    // GET system/v1/users/{userName}
     /// <summary>
     ///     Returns user information based on it's userName, email or id
     /// </summary>
@@ -103,6 +104,28 @@ public class IdentitiesController : ControllerBase
     [Authorize(IdentityServiceConstants.IdentityApiReadOnlyPolicy)]
     public async Task<IActionResult> Get([Required] string userName)
     {
+        var rtUser = await _userManager.FindByNameAsync(userName) ??
+                     await _userManager.FindByEmailAsync(userName) ??
+                     await _userManager.FindByIdAsync(userName);
+        if (rtUser == null)
+        {
+            return NotFound();
+        }
+
+        var userDto = _mapper.Map<UserDto>(rtUser);
+        return Ok(userDto);
+    }
+
+    // GET system/v1/users/{userName}
+    /// <summary>
+    ///     Returns user information based on it's userName, email or id
+    /// </summary>
+    /// <param name="userName">Name of the user</param>
+    /// <returns>An Object that describes the user.</returns>
+    [HttpGet("{userName}/roles")]
+    [Authorize(IdentityServiceConstants.IdentityApiReadOnlyPolicy)]
+    public async Task<IActionResult> GetUserRoles([Required] string userName)
+    {
         var octoUser = await _userManager.FindByNameAsync(userName) ??
                        await _userManager.FindByEmailAsync(userName) ??
                        await _userManager.FindByIdAsync(userName);
@@ -111,11 +134,25 @@ public class IdentitiesController : ControllerBase
             return NotFound();
         }
 
-        return Ok(CreateUserDto(octoUser));
+
+        List<RoleDto> roles = new();
+        if (octoUser.RoleIds != null)
+        {
+            foreach (var octoUserRoleId in octoUser.RoleIds)
+            {
+                var role = await _roleManager.FindByIdAsync(octoUserRoleId);
+                if (role != null)
+                {
+                    roles.Add(_mapper.Map<RoleDto>(role));
+                }
+            }
+        }
+
+        return Ok(roles);
     }
 
 
-    // POST system/v1/identities
+    // POST system/v1/users
     /// <summary>
     ///     Creates a new user
     /// </summary>
@@ -130,20 +167,11 @@ public class IdentitiesController : ControllerBase
             return BadRequest(ModelState);
         }
 
-        var applicationUser = new RtUser();
+        var rtUser = _mapper.Map<RtUser>(userDto);
 
         try
         {
-            await ApplyRegisterUserToUser(applicationUser, userDto);
-        }
-        catch (RoleNotFoundException e)
-        {
-            return BadRequest(new OperationFailedError(e.Message));
-        }
-
-        try
-        {
-            var result = await _userManager.CreateAsync(applicationUser);
+            var result = await _userManager.CreateAsync(rtUser);
 
             if (!result.Succeeded)
             {
@@ -154,8 +182,8 @@ public class IdentitiesController : ControllerBase
 
             if (!string.IsNullOrWhiteSpace(userDto.Password))
             {
-                var token = await _userManager.GeneratePasswordResetTokenAsync(applicationUser);
-                result = await _userManager.ResetPasswordAsync(applicationUser, token, userDto.Password);
+                var token = await _userManager.GeneratePasswordResetTokenAsync(rtUser);
+                result = await _userManager.ResetPasswordAsync(rtUser, token, userDto.Password);
 
                 if (!result.Succeeded)
                 {
@@ -163,11 +191,11 @@ public class IdentitiesController : ControllerBase
                     return GetBadRequestResultWithErrorDescription("Creation of user failed", result.Errors);
                 }
 
-                await _userEmailInteractionService.SendWelcomeNotificationAsync(applicationUser);
+                await _userEmailInteractionService.SendWelcomeNotificationAsync(rtUser);
             }
             else
             {
-                await _userEmailInteractionService.SendWelcomeNotificationWithoutPasswordAsync(applicationUser);
+                await _userEmailInteractionService.SendWelcomeNotificationWithoutPasswordAsync(rtUser);
             }
 
             return Ok();
@@ -178,7 +206,7 @@ public class IdentitiesController : ControllerBase
         }
     }
 
-    // PUT system/v1/identities/5
+    // PUT system/v1/users/5
     /// <summary>
     ///     Updates an user
     /// </summary>
@@ -194,24 +222,17 @@ public class IdentitiesController : ControllerBase
             return BadRequest(ModelState);
         }
 
-        var applicationUser = await _userManager.FindByNameAsync(userName);
-        if (applicationUser == null)
+        var rtUser = await _userManager.FindByNameAsync(userName);
+        if (rtUser == null)
         {
             return NotFound(new NotFoundError($"User name '{userName}' not found."));
         }
 
-        try
-        {
-            ApplyUserToUserDto(applicationUser, userDto);
-        }
-        catch (RoleNotFoundException e)
-        {
-            return BadRequest(new OperationFailedError(e.Message));
-        }
+        _mapper.Map(userDto, rtUser);
 
         try
         {
-            var result = await _userManager.UpdateAsync(applicationUser);
+            var result = await _userManager.UpdateAsync(rtUser);
             if (result.Succeeded)
             {
                 return Ok();
@@ -225,7 +246,7 @@ public class IdentitiesController : ControllerBase
         }
     }
 
-    // POST: system/v1/identities/resetPassword
+    // POST: system/v1/users/resetPassword
     /// <summary>
     ///     Resets the password of an user
     /// </summary>
@@ -267,7 +288,7 @@ public class IdentitiesController : ControllerBase
         }
     }
 
-    // DELETE system/v1/identities/5
+    // DELETE system/v1/users/5
     /// <summary>
     ///     Deletes an user
     /// </summary>
@@ -304,7 +325,42 @@ public class IdentitiesController : ControllerBase
         }
     }
 
-    // PUT system/v1/identities/demo/roles/users
+    // PUT system/v1/users/demo/roles/users
+    /// <summary>
+    ///     adds a role from an user
+    /// </summary>
+    /// <param name="userName">The user name</param>
+    /// <param name="roleIds">The role ids</param>
+    /// <returns></returns>
+    [HttpPut("{userName}/roles")]
+    [Authorize(IdentityServiceConstants.IdentityApiReadWritePolicy)]
+    public async Task<IActionResult> UpdateUserRoles([Required] string userName, [Required] [FromBody] IEnumerable<OctoObjectId> roleIds)
+    {
+        var user = await _userManager.FindByNameAsync(userName);
+        if (user == null)
+        {
+            return NotFound(new NotFoundError($"User with name '{userName}' not found."));
+        }
+
+        user.RoleIds ??= new AttributeStringValueList();
+        user.RoleIds.Clear();
+
+        foreach (var roleId in roleIds)
+        {
+            var role = await _roleManager.FindByIdAsync(roleId.ToString());
+            if (role == null)
+            {
+                return NotFound(new NotFoundError($"Role with id '{roleId}' not found."));
+            }
+
+            user.RoleIds?.Add(role.RtId.ToString());
+        }
+
+        await _userManager.UpdateAsync(user);
+        return Ok();
+    }
+
+    // PUT system/v1/users/demo/roles/users
     /// <summary>
     ///     adds a role from an user
     /// </summary>
@@ -338,7 +394,7 @@ public class IdentitiesController : ControllerBase
         return Ok();
     }
 
-    // DELETE system/v1/identities/demo/roles/Users
+    // DELETE system/v1/users/demo/roles/Users
     /// <summary>
     ///     removes a role from an user
     /// </summary>
@@ -369,65 +425,6 @@ public class IdentitiesController : ControllerBase
         user.RoleIds?.Remove(role.RtId.ToString());
         await _userManager.UpdateAsync(user);
         return Ok();
-    }
-
-    private UserDto CreateUserDto(RtUser applicationUser)
-    {
-        var userDto = new UserDto
-        {
-            UserId = applicationUser.RtId.ToString(),
-            FirstName = applicationUser.FirstName,
-            LastName = applicationUser.LastName,
-            Email = applicationUser.Email ??
-                    applicationUser.Claims?.FirstOrDefault(x => x.ClaimType == JwtClaimTypes.Email)?.ClaimValue,
-            Name = applicationUser.UserName ??
-                   applicationUser.Claims?.FirstOrDefault(x => x.ClaimType == JwtClaimTypes.Name)?.ClaimValue
-        };
-
-        return userDto;
-    }
-    
-    private async Task ApplyRegisterUserToUser(RtUser octoUser, RegisterUserDto userDto)
-    {
-        var roleIds = new List<string>();
-
-        if (userDto.Roles != null)
-        {
-            foreach (var roleDto in userDto.Roles)
-            {
-                if (roleDto.Id == null)
-                {
-                    continue;
-                }
-
-                var role = await _roleManager.FindByIdAsync(roleDto.Id);
-                if (role == null)
-                {
-                    throw new RoleNotFoundException($"Role '{roleDto.Name}' does not exist.");
-                }
-
-                roleIds.Add(role.RtId.ToString());
-            }
-        }
-
-        ApplyUserToUserDto(octoUser, userDto);
-
-        octoUser.RoleIds ??= new AttributeStringValueList();
-        octoUser.RoleIds?.Clear();
-        octoUser.RoleIds?.AddRange(roleIds);
-    }
-
-    private void ApplyUserToUserDto(RtUser octoUser, UserDto userDto)
-    {
-        if (!string.IsNullOrWhiteSpace(userDto.Name))
-        {
-            octoUser.UserName = userDto.Name;
-        }
-
-        octoUser.Email = userDto.Email;
-        octoUser.FirstName = userDto.FirstName;
-        octoUser.LastName = userDto.LastName;
-        octoUser.ResetPasswordOnLogin = userDto.ResetPasswordOnLogin;
     }
 
     private void LogIdentityError(string operation, IEnumerable<IdentityError> errors)
